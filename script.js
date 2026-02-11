111c4f709051f0465b8c0c8154d2d553d4e3ca22
Zhang Zhe Xin <zhexinnn@gmail.com>
2026-02-12 04:53:45 +0800

    Fix styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let leftPos = 0
 let bottomPos = 0
 document.getElementById('yessir').addEventListener('click', () => {
-    alert('对你面的女孩看过来👀!\n The annoying Zhang Zhe Xin can\'t wait to see you on the 14th Feb from 515pm to 1215am! Prepare your heart and dress comfortably because you will be in for a night full of surprises and fun 🤫😚🐼 \nUh oh! There will be one every hour 🔥')
+    alert('对你面的女孩看过来👀!\nThe annoying Zhang Zhe Xin can\'t wait to see you on the 14th Feb from 515pm to 1215am! Prepare your heart and dress comfortably because you will be in for a night full of surprises and fun 🤫😚🐼 \nUh oh! There will be one every hour 🔥')
 })
 document.getElementById('nooo').addEventListener('mouseover', () => {
     leftPos = Math.floor(Math.random() * (50 - (-50) + 1)) + (-50);
